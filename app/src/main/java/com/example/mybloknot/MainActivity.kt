ebff99f7b6d969d0ab73632df3dbbfba0a7b5958
Talanov-Yan <yan.talanov@bk.ru>
2021-05-25 18:17:26 +0300

    Подключили адаптер

--- app/src/main/java/com/example/mybloknot/MainActivity.kt
+++ app/src/main/java/com/example/mybloknot/MainActivity.kt
@@ -4,21 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mybloknot.databinding.ActivityMainBinding
+import com.example.mybloknot.db.MyAdapter
 import com.example.mybloknot.db.MyDbManager
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClassMain: ActivityMainBinding
+
+    val myAdapter = MyAdapter(ArrayList())
     var myDbManager = MyDbManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClassMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClassMain.root)
+
+        init()
     }
     override fun onResume() {
         super.onResume()
         myDbManager.openDb()
+        fillAdapter()
 
     }
 
@@ -30,5 +37,14 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         myDbManager.closeDb()
+    }
+
+    private fun init() {        // инициализируем recycler view
+        bindingClassMain.rcView.layoutManager = LinearLayoutManager(this) // элементу будут распологаться по вертикале
+        bindingClassMain.rcView.adapter = myAdapter
+    }
+
+    fun fillAdapter() {
+        myAdapter.upDataAdapter(myDbManager.readDbData())
     }
 }
